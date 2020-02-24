4d612b40c7bbd08f7ab002247d3dacd6e83797d2
Artemiy <aaegorof@gmail.com>
2020-02-24 20:43:45 +0300

    Direction page added About page added

--- src/components/directions.jsx
+++ src/components/directions.jsx
@@ -1,8 +1,13 @@
 import React from "react"
 
 const Directions = () => {
-
-  return <div className="Directions-wrap"></div>
+  const directions = ["Финансовый сектор", "Государственный сектор", "Промышленный сектор", "Цифровой сектор"]
+  return <div className="directions-wrap mg-xs-3-t mg-lg-5-t">
+    {directions.map( (item, idx) => <div className="sector-item" key={idx}>
+      <div className="number">{String(idx+1).padStart(2,0)}</div>
+      {item}
+    </div>)}
+  </div>
 }
 
 export default Directions
